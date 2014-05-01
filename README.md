d8c98ba5eaaf531b6810715a42e5a1339de00430
Anthony Zacharakis <anthony@lumoslabs.com>
2014-04-30 21:02:20 -0700

    Modify tests to work with SQLAlchemy

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # This app is built on Flask and Backbone.
 
-To start:
+## To start:
 
 * cd into the todo workspace
 * Create your database in a python console with:
@@ -11,3 +11,9 @@ db.create_all()
 * Install dependencies with `pip install -r requirements.txt`
 * Run `python todo.py` from the command line.
 * Navigate to http://127.0.0.1:5000/ in a browser.
+
+## To run tests:
+
+* cd into the todo workspace
+* Install dependencies with `pip install -r requirements.txt`
+* Run `python todo_tests.py` from the command line.
--- todo_tests.py
+++ todo_tests.py
@@ -1,26 +1,38 @@
-import os
+import json
 import todo
 import unittest
-import tempfile
+
+from todo import db, Task
 
 class TodoTestCase(unittest.TestCase):
 
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/todo_test.db'
+
     def setUp(self):
-        self.db_fd, todo.app.config['DATABASE'] = tempfile.mkstemp()
         todo.app.config['TESTING'] = True
         self.app = todo.app.test_client()
-        todo.init_db()
+        db.create_all()
 
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(todo.app.config['DATABASE'])
+        db.session.remove()
+        db.drop_all()
 
     def test_index_responds_successfully(self):
       assert '200 OK' in self.app.get('/').status
 
     def test_tasks_index_with_no_tasks(self):
         rv = self.app.get('/tasks')
-        assert [] == json.loads(rv.data)
+        assert {"tasks": []} == json.loads(rv.data)
+
+    def test_tasks_index_with_tasks(self):
+        task_1 = Task('some task', 0)
+        task_2 = Task('some other task', 1)
+        db.session.add(task_1)
+        db.session.add(task_2)
+        db.session.commit()
+
+        rv = self.app.get('/tasks')
+        assert {"tasks": [task_1.to_json, task_2.to_json]} == json.loads(rv.data)
 
 if __name__ == '__main__':
     unittest.main()
