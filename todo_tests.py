89e45b9808e4f50016129be3e2872bab179cfdc5
Anthony Zacharakis <anthony@lumoslabs.com>
2014-05-02 17:09:08 -0700

    Add tests for reordering tasks

--- todo_tests.py
+++ todo_tests.py
@@ -34,9 +34,9 @@ class TodoTestCase(unittest.TestCase):
         assert [task_1.to_json, task_2.to_json] == json.loads(rv.data)
 
     def test_tasks_index_with_ordered_tasks(self):
-        task_3 = Task('some task', False, 3)
-        task_2 = Task('some other task', True, 2)
-        task_1 = Task('some other task', True, 1)
+        task_3 = Task('some task', False, 2)
+        task_2 = Task('some other task', True, 1)
+        task_1 = Task('some other task', True, 0)
         db.session.add(task_3)
         db.session.add(task_2)
         db.session.add(task_1)
@@ -68,6 +68,30 @@ class TodoTestCase(unittest.TestCase):
 
         rv = self.app.put('/tasks/' + str(task.id), content_type='application/json', content_length=16, data=json.dumps({'completed': False}))
         assert {"completed": False, "id": 1, "text": "some task", "position": 0} == json.loads(rv.data)
+
+    def test_tasks_reordering_existing_task_response(self):
+        task_1 = Task('task 1', False, 0)
+        task_2 = Task('task 2', False, 1)
+        db.session.add(task_1)
+        db.session.add(task_2)
+        db.session.commit()
+
+        rv = self.app.put('/tasks/' + str(task_1.id), content_type='application/json', content_length=11, data=json.dumps({'position': 1}))
+        assert {"completed": False, "id": 1, "text": "task 1", "position": 1} == json.loads(rv.data)
+
+    def test_tasks_reordering_existing_task(self):
+        task_1 = Task('task 1', False, 0)
+        task_2 = Task('task 2', False, 1)
+        task_3 = Task('task 3', False, 2)
+        task_4 = Task('task 4', False, 3)
+        db.session.add(task_1)
+        db.session.add(task_2)
+        db.session.add(task_3)
+        db.session.add(task_4)
+        db.session.commit()
+
+        rv = self.app.put('/tasks/' + str(task_3.id), content_type='application/json', content_length=11, data=json.dumps({'position': 1}))
+        assert [0, 2, 1, 3] == map(lambda task: task.position, Task.query.order_by(Task.id.asc()).all())
 
 if __name__ == '__main__':
     unittest.main()
